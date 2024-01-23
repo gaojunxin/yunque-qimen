3fb2413e575533ba54977cb7d589c377f843fb7d
gaojunxin <gaojunxin@users.noreply.github.com>
2024-01-23 06:13:11 +0000

    deploy: 39455a3fce1b6f99db791bdf502fb0ebbd881dea

--- assets/AboutView--uAKzMUg.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as o,o as s,a as t}from"./index-nhhDg4aq.js";const c={},a={class:"about"},n=t("h1",null,"This is an about page",-1),_=[n];function r(i,d){return s(),o("div",a,_)}const u=e(c,[["render",r]]);export{u as default};
